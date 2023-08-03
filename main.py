546003203cd7fffeec41a652b6ea9b99038b9170
Aditya S <asrinivas2002@gmail.com>
2023-08-03 18:45:00 +0530

    Fixed functionality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+import json
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from google.oauth2.credentials import Credentials
@@ -8,8 +9,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 
 
 api_key = os.getenv('YT_API_KEY')
-SCOPES = ["https://www.googleapis.com/auth/youtubepartner"]
-
+SCOPES = ["https://www.googleapis.com/auth/youtube.force-ssl"]
 
 
 def build_youtube_client():
@@ -40,6 +40,8 @@ def build_youtube_client():
     return build(api_service_name, api_version, credentials=credentials)
 
 
+
+# Function to get playlist id from url
 def get_playlist_id(playlist_url):
     client = build('youtube', 'v3', developerKey=api_key)
 
@@ -53,8 +55,7 @@ def get_playlist_id(playlist_url):
 
     if playlist_id:
 
-        playlist_response = client.playlists().list(part='snippet',
-                                                    id=playlist_id).execute()
+        playlist_response = client.playlists().list(part='snippet',id=playlist_id).execute()
 
         if 'items' in playlist_response and len(
                 playlist_response['items']) > 0:
@@ -70,6 +71,8 @@ def get_playlist_id(playlist_url):
     return playlist_id
 
 
+#match playlist with keywords
+
 def get_matching_videos(playlist_id, keywords):
     youtube = build_youtube_client()
     video_data = []
@@ -77,11 +80,11 @@ def get_matching_videos(playlist_id, keywords):
     try:
         next_page_token = None
         while True:
-            request = youtube.playlistItems().list(part="snippet",
+            request = youtube.playlistItems().list(part="snippet,contentDetails",
+                                                   maxResults=30, #change accordingly
                                                    playlistId=playlist_id,
-                                                   maxResults=70,
                                                    pageToken=next_page_token)
-            
+
             response = request.execute()
 
             for item in response["items"]:
@@ -89,7 +92,8 @@ def get_matching_videos(playlist_id, keywords):
                 video_id = item["snippet"]["resourceId"]["videoId"]
 
                 if any(keyword in video_title for keyword in keywords):
-                    video_data.append({"title": video_title, "id": video_id})
+                    formatted_video = {"title": video_title, "id": video_id}
+                    video_data.append(formatted_video)
 
             next_page_token = response.get("nextPageToken")
             if not next_page_token:
@@ -97,6 +101,10 @@ def get_matching_videos(playlist_id, keywords):
 
     except HttpError as e:
         print("An HTTP error occurred:", e)
+
+    # with open("test.txt","w") as file:
+    #     for video in video_data:
+    #         file.write(str(video) + "\n")
 
     return video_data
 
@@ -109,14 +117,14 @@ def add_videos_to_playlist(your_playlist_id, video_data):
     try:
         for video in video_data:
             request = youtube.playlistItems().insert(
-                part="status",
+                part="snippet",
                 body={
-                    "status": {
+                    "snippet": {
                         "playlistId": your_playlist_id,
                         "position": 0,
                         "resourceId": {
                             "kind": "youtube#video",
-                            "videoId": video["id"]
+                            "videoId": video["id"] # change
                         }
                     }
                 })
@@ -128,60 +136,31 @@ def add_videos_to_playlist(your_playlist_id, video_data):
         print("An HTTP error occurred:", e)
 
 
-
-playlist_id = "PLVDfFatHsysT6JHrHfmkFd_XSuzF4K_CQ"
-
-keywords = ["18ec63", "18EC63"]
-
-personal_playlist_id = "PLKyn_6NQ269xncCmHR4zCIg-eoEXMn1fk"
-
-matching_videos = get_matching_videos(playlist_id, keywords)
-
-add_videos_to_playlist(personal_playlist_id, matching_videos)
-
-
-# # Function to add videos to a new playlist
-# def add_videos_to_playlist(playlist_id, video_data):
-#     youtube = build_youtube_client()
-
-#     try:
-#         for video in video_data:
-#             request = youtube.playlistItems().insert(
-#                 part="snippet",
-#                 body={
-#                     "snippet": {
-#                         "playlistId": playlist_id,
-#                         "position": 0,
-#                         "resourceId": {
-#                             "kind": "youtube#video",
-#                             "videoId": video["id"]
-#                         }
-#                     }
-#                 })
-#             request.execute()
-
-#         print("Videos added to the new playlist successfully!")
-
-#     except HttpError as e:
-#         print("An HTTP error occurred:", e)
-
-
-
-
 # Example usage
-# def main():
-#     playlist_url = input("Enter the YouTube playlist URL: ")
-#     channel_playlist_id = get_playlist_id(playlist_url=playlist_url)
+def main():
 
-#     your_playlist_url = input("Enter Your private YouTube playlist URL: ")
-#     personal_playlist_id = get_playlist_id(playlist_url=your_playlist_url)
+    channel_playlist_id = "PLVDfFatHsysT6JHrHfmkFd_XSuzF4K_CQ"
+    your_playlist_id = "PLKyn_6NQ269yFJZOYwM-HCAjWou5c1f0S"
 
-#     keyword = input("Enter the keyword you want to search for in the video titles and descriptions: ")  # add more functionality
+    keywords = '18EC63'
 
+    videos = get_matching_videos(channel_playlist_id,keywords=keywords)
 
-#     videos = get_matching_videos(channel_playlist_id, keywords=keyword)
-#     add_videos_to_playlist(your_playlist_id=personal_playlist_id,video_data=videos)
+    add_videos_to_playlist(your_playlist_id,video_data= videos)
 
 
-# if __name__ == "__main__":
-#     main()
+    # playlist_url = input("Enter the YouTube playlist URL: ")
+    # channel_playlist_id = get_playlist_id(playlist_url=playlist_url)
+
+    # your_playlist_url = input("Enter Your private YouTube playlist URL: ")
+    # personal_playlist_id = get_playlist_id(playlist_url=your_playlist_url)
+
+    # keyword = input("Enter the keyword you want to search for in the video titles and descriptions: ")  # add more functionality
+
+
+    # videos = get_matching_videos(channel_playlist_id, keywords=keyword)
+    # add_videos_to_playlist(your_playlist_id=personal_playlist_id,video_data=videos)
+
+
+if __name__ == "__main__":
+    main()
